1d51747e370b3fd4041c521c70b10a9e51197c34
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-11-24 16:30:58 +0100

    nuevo comando

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
 require("dotenv").config();
 
-// Cargamos el archivo profiles.json
+//  profiles.json
 let perfiles = JSON.parse(fs.readFileSync("profiles.json", "utf8"));
 
 const client = new Client({
@@ -17,41 +17,48 @@ client.once("ready", () => {
     console.log(`Bot conectado como ${client.user.tag}`);
 });
 
-// Comando !ping
+// Escucha todos los mensajes
 client.on("messageCreate", (message) => {
+
     if (message.content === "!ping") {
         message.reply("pong");
     }
-});
 
-// Comando !elo
-client.on("messageCreate", (message) => {
-    if (!message.content.startsWith("!elo")) return;
+    // Comando !elo
+    if (message.content.startsWith("!elo")) {
+        const mencionado = message.mentions.users.first();
 
-    const mencionado = message.mentions.users.first();
-
-    // !elo general
-    if (!mencionado) {
-        const links = [
-            "https://op.gg/lol/summoners/euw/TINYX1-EUW",
-            "https://op.gg/lol/summoners/euw/iK3y-EUW",
-            "https://op.gg/lol/summoners/euw/Euphoria1404-EUW"
-        ];
-
-        message.reply("Aquí están los 3 perfiles mi rey:\n" + links.join("\n"));
-        return;
+        // !elo general
+        if (!mencionado) {
+            const links = [
+                "https://op.gg/lol/summoners/euw/TINYX1-EUW",
+                "https://op.gg/lol/summoners/euw/iK3y-EUW",
+                "https://op.gg/lol/summoners/euw/Euphoria1404-EUW"
+            ];
+    
+            message.reply("Aquí están los 3 perfiles mi rey:\n" + links.join("\n"));
+            return;
+        }
+    
+        // !elo @usuario
+        const riotID = perfiles[mencionado.id];
+    
+        if (!riotID) {
+            message.reply(`${mencionado.username} no tiene Riot ID registrado.`);
+            return;
+        }
+    
+        const link = `https://op.gg/lol/summoners/euw/${encodeURIComponent(riotID)}`;
+        message.reply(`Mi Rey aquí tienes el elo de este aweonao llamado: " ${mencionado.username} ":\n${link}`);
     }
 
-    // Caso !elo @usuario
-    const riotID = perfiles[mencionado.id];
-
-    if (!riotID) {
-        message.reply(`${mencionado.username} no tiene Riot ID registrado.`);
-        return;
+    // Comando !jorge
+    if (message.content === "!jorge") {
+        const datos = fs.readFileSync("./datos.txt", "utf8").split("\n").filter(Boolean);
+        const random = datos[Math.floor(Math.random() * datos.length)];
+        message.channel.send(random);
     }
 
-    const link = `https://op.gg/lol/summoners/euw/${encodeURIComponent(riotID)}`;
-    message.reply(`Mi Rey aquí tienes el elo de este aweonao llamado: " ${mencionado.username} ":\n${link}`);
 });
 
 // Login
